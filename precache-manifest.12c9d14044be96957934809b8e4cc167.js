488b38052a39971182a4a73324a43ab2f0bc3243
Sijuade Ajagunna <mahyor.sam@gmail.com>
2019-12-09 22:24:18 -0800

    Updates

---
+++ precache-manifest.12c9d14044be96957934809b8e4cc167.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "236c19ddb50cf0b27878ee7f5f83f3fd",
+    "url": "/police/index.html"
+  },
+  {
+    "revision": "6ed67069a325e1582453",
+    "url": "/police/static/css/main.c3519527.chunk.css"
+  },
+  {
+    "revision": "da468cce374aa199fce6",
+    "url": "/police/static/js/2.087f4f5b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/police/static/js/2.087f4f5b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6ed67069a325e1582453",
+    "url": "/police/static/js/main.3aa2f83a.chunk.js"
+  },
+  {
+    "revision": "4b5c574e5c3828a23a2e",
+    "url": "/police/static/js/runtime-main.a813aaa7.js"
+  },
+  {
+    "revision": "7d53597ad186fccf04abd1dfe1bd9d18",
+    "url": "/police/static/media/logo.7d53597a.jpg"
+  }
+]);
